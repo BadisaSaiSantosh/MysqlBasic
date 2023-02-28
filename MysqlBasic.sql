b6538453e0a21c7fda268e34104a1b8a8de5c5ea
BadisaSaiSantosh <96568872+BadisaSaiSantosh@users.noreply.github.com>
2023-02-28 12:51:00 +0530

    basic commands on my sql to find the price of products between 20rs && 50rs... based on w3 schools database

---
+++ MysqlBasic.sql
@@ -0,0 +1,8 @@
+SELECT * FROM [Products] WHERE Price<=50 AND Price>=10;
+
+/*
+ProductID	ProductName	SupplierID	CategoryID	      Unit	                Price
+1	Chais	1	            1	       10 boxes  x   20 bags 	               18
+2	Chang	1	            1	        24        -  12 oz bottles	           19
+3	Anise	1	            2	        12       -   550 ml bottles	           10
+4	Chef 	2            	2	        48      -     6 oz jars             	22
